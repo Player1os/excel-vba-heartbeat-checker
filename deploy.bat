d8d98a851952d5c7f5e0253f96e5dd410b8a22ea
Osama Hassanein <osama.hassanein@telekom.sk>
2019-02-02 12:58:04 +0100

    Implement the deploy script and allow for non local execution.

---
+++ deploy.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Set the deploy directory path environment variable.
+set DEPLOY_DIRECTORY_PATH=H:\projects\heartbeat-checker
+
+:: Create a fresh deploy directory at the specified path.
+if exist "%DEPLOY_DIRECTORY_PATH%\" rd /s /q "%DEPLOY_DIRECTORY_PATH%"
+mkdir "%DEPLOY_DIRECTORY_PATH%"
+
+:: Copy all necessary files to the deploy directory.
+copy /y "%~dp0emit.bat" "%DEPLOY_DIRECTORY_PATH%\" >nul
+copy /y "%~dp0emit.vbs" "%DEPLOY_DIRECTORY_PATH%\" >nul
+copy /y "%~dp0verify.bat" "%DEPLOY_DIRECTORY_PATH%\" >nul
+copy /y "%~dp0verify.vbs" "%DEPLOY_DIRECTORY_PATH%\" >nul
+
+:: Unset the deploy directory path environment variable.
+set DEPLOY_DIRECTORY_PATH=
--- verify.bat
+++ verify.bat
@@ -1,4 +1,4 @@
 @echo off
 
 :: Execute the verify vbs script, reading the contents of the predetermined heartbeat checker timestamp file.
-cscript /NoLogo "verify.vbs" <"%HEARTBEAT_CHECKER_TIMESTAMP_FILE_PATH%"
+cscript /NoLogo "%~dp0verify.vbs" <"%HEARTBEAT_CHECKER_TIMESTAMP_FILE_PATH%"
